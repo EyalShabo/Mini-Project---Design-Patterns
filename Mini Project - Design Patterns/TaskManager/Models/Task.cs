147bc382c844b3feacfeb20a3e64524f48da6e71
EyalShabo <eyal.shabo.mail@gmail.com>
2025-05-15 20:26:29 +0300

    Start Of the Repository And Make the Task Class

---
+++ Mini Project - Design Patterns/TaskManager/Models/Task.cs
@@ -0,0 +1,13 @@
+﻿namespace Mini_Project___Design_Patterns.TaskManager.Models;
+
+public sealed class Task {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime DueDate { get; set; }
+
+    public Task(string title, string description, DateTime dueDate) {
+        Title = title;
+        Description = description;
+        DueDate = dueDate;
+    }
+}
---
+++ Mini Project - Design Patterns/TaskManager/Repository/TaskRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Mini_Project___Design_Patterns.TaskManager.Repository;
+
+public class TaskRepository {
+    private static TaskRepository _instance = new TaskRepository();
+    private List<Task> _tasks = new List<Task>();
+    
+    private TaskRepository() {}
+    
+    public static TaskRepository GetInstance() => _instance;
+
+    public void AddTask(Task task) => _tasks.Add(task);
+    public void DeleteTask(int index) => _tasks.RemoveAt(index);
+    public void UpdateTask(int index, Task task) => _tasks[index] = task;
+    public List<Task> GetTasks() => _tasks;
+}
